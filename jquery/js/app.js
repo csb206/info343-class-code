98216a50074529c1faa8a2ab9c6e3ba21fb365ce
Cory Brown <cory_s_brown@msn.com>
2015-10-15 17:12:25 -0700

    more javascript

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -5,4 +5,22 @@
     open weather API: 
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
+// will run when DOM content has been loaded...
+$(function() {
+    'use strict';
+    $('a').attr('target', 'blank');
+    $('article').hide().fadeIn(2000);
+    $('#toggle-article').click(function() {
+        $('article').fadeToggle();
+    });
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
 
+    $.getJSON(url).then(function(data) {
+        console.log(data);
+        var temperature = data.main.temp;
+        //this sets the id = temp to the temperature data
+        $('#temp').text(Math.round(temperature));
+    });
+    console.log('Test');
+
+});
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,32 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+$(function() {
+    'user strict';
+    function createMap(loc, zoom) {
+
+        var map = L.map('map').setView(loc, zoom);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.circleMarker(loc).addTo(map).bindPopup('<h2>Hello></h2>');
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+           createMap([pos.coords.latitude, pos.coords.longitude], 15);
+        });
+    } else {
+        createMap([47.6097, -122.3331], 12);
+    }
+})
+
+
+//get json
+//iterate over it using the .foreach
+//each element in array has properties
+//two most important are latitude and logitutde
+//adding markers to a map per camera
+//who owns the camera, the wdot or sdot
+//bindpopup pops up with the marker after you click it. it can be any html, img tag picture requested from server
