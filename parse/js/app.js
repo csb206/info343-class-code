9985215dbdaa7a6405f49b540bed5980d1362de6
Cory Brown <cory_s_brown@msn.com>
2015-10-20 17:15:49 -0700

    in class using parse

--- parse/js/app.js
+++ parse/js/app.js
@@ -1,3 +1,84 @@
 /*
     script for the index.html file
 */
+
+Parse.initialize("x4GVtocaRnwlz9JWksKwLpvNwuJuNYbtaNuYPCgi", "rwrZBa0ASplceS9563xxmZM438jvdtJZoNfbVOPV");
+$(function() {
+    'use strict';
+
+    // new Task class for Parse
+    var Task = Parse.Object.extend('Task');
+    // new query that will return all tasks ordered by createAt
+    var tasksQuery = new Parse.Query(Task);
+    tasksQuery.ascending('createdAt');
+
+    // reference to the task list element
+    var tasksList = $('#tasks-list');
+
+    // reference to the error message alert
+    var errorMessage = $('#error-message');
+
+    // current set of tasks
+    var tasks = [];
+
+    function displayError(err) {
+        errorMessage.text(err.message);
+        errorMessage.fadeIn();
+
+    }
+
+    function clearError() {
+        errorMessage.hide();
+    }
+
+    function showSpinner() {
+        $('.fa-spin').show();
+    }
+
+    function hideSpinner() {
+        $('.fa-spin').hide();
+    }
+
+    function fetchTasks() {
+        showSpinner();
+        // dont need parameters for either function
+        tasksQuery.find()
+            .then(onData, displayError)
+            .always(hideSpinner);
+    }
+
+    function onData(results) {
+        tasks = results;
+        renderTasks();
+    }
+
+    function renderTasks() {
+        tasksList.empty();
+        tasks.forEach(function(task) {
+            // following three are one line
+            $(document.createElement('li'))
+                .text(task.get('title'))
+                .appendTo(tasksList);
+        });
+    }
+
+    $('#new-task-form').submit(function(evt) {
+        // refreshes page itself
+       evt.preventDefault();
+        var titleInput = $(this).find('[name="title"]');
+        var title = titleInput.val();
+        var task = new Task();
+        task.set('title', title);
+        task.save().then(fetchTasks, displayError).then(function() {
+            titleInput.val('');
+        });
+
+
+        return false;
+    });
+
+    // go and fetch tasksfrom Parse
+    fetchTasks();
+
+    window.setInterval(fetchTasks, 3000);
+});
