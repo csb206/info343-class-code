bf1dc819d6f76ca6d7ff833a467f57a5fa0ee0e9
Cory Brown <cory_s_brown@msn.com>
2015-10-27 17:17:02 -0700

    angular classwork

--- angular/js/tasks.js
+++ angular/js/tasks.js
@@ -1,4 +1,30 @@
 /* 
     script for the tasks.html file 
 */
+angular.module('Tasks', [])
+    contant('tasksKey', 'tasks'); // settomg taskkey to tasks
+    .controller('TasksController', function($scope, tasksKey) {
+        // intialize tasks property on the scope to an empty array
+        $scope.tasks = angular.fromJson(localStorage.getItem(tasksKey)) || [];    // empty array
+        // initilize newTask to an empty object
+        $scope.newTask = {};  // empty object
 
+        function saveTasks() {
+            localStorage.setItem(tasksKey, angular.toJson($scope.tasks));
+        }
+
+        // add a function to add newTask to the array
+        $scope.addTask = function() {
+            // push the current value of newTask into the tasks
+            $scope.tasks.push($scope.newTask);
+            // save the tasks
+            saveTasks();
+            // reset newTask to an empty object
+            $scope.newTask = {}; // clears out edit box
+        };
+
+        $scope.toggleDone = function(task) {
+            task.done = !task.done;
+            saveTasks();
+        }
+    });
