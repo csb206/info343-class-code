6c4ecc2f4963ea6f582e03e7910555e069e4b525
Cory Brown <cory_s_brown@msn.com>
2015-11-05 16:58:47 -0800

    more angular class

--- angular-forms/js/app.js
+++ angular-forms/js/app.js
@@ -5,13 +5,7 @@
 angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'])
     .constant('storageKey', 'contacts-list')
     .factory('contacts', function(uuid, localStorageService, storageKey) {
-        return [{
-            id: 'default-delete-me',
-            fname: 'Buzz',
-            lname: 'Lightyear',
-            phone: '206-781-0187',
-            dob: '1/1/1998'
-        }];
+        return localStorageService.get(storageKey) || [];
     })
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
@@ -33,6 +27,20 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $urlRouterProvider.otherwise('/contacts');
     })
 
+    // register a directive for custome validation of dates in the past
+    .directive('inThePast', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elem, attrs, controller) {
+                controller.$validators.inThePast = function(modelValue) {
+                    var today = new Date();
+                    return (new Date(modelValue) <= today);
+                }
+            }
+        }
+
+    })
+
     .controller('ContactsController', function($scope, contacts) {
         $scope.contacts = contacts;
     })
@@ -44,8 +52,8 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         });
     })
 
-    .controller('EditContactController', function($scope, $stateParams, $state, contacts) {
-
+    .controller('EditContactController', function($scope, $stateParams, $state,
+                                                  uuid, localStorageService, storageKey, contacts) {
         var existingContact = contacts.find(function (contact) {
             return contact.id === $stateParams.id; // SP id matches contacts/id and contactID matches the factory id
 
@@ -54,7 +62,13 @@ angular.module('ContactsApp', ['ui.router', 'angular-uuid', 'LocalStorageModule'
         $scope.contact = angular.copy(existingContact);
 
         $scope.save = function() {
-            angular.copy($scope.contact, existingContact);
+            if ($scope.contact.id) {
+                angular.copy($scope.contact, existingContact);
+            } else {
+                $scope.contact.id = uuid.v4();
+                contacts.push($scope.contact);
+            }
+            localStorageService.set(storageKey, contacts);
             $state.go('list');
-        }
+        };
     });
--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,6 +1,21 @@
 
 angular.module('ChatApp', ['firebase'])
     .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope) {
+    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
         //TODO: implement our chat controller
+        var ref = new Firebase(firebaseUrl);
+        $scope.messages = $firebaseArray(ref);
+
+        $scope.name = null;
+        $scope.body = null;
+
+        $scope.sendMessage = function() {
+            // adds
+            $scope.messages.$add({
+                name: $scope.name,
+                body: $scope.body,
+                createdAt: Firebase.ServerValue.TIMESTAMP
+            });
+            $scope.body = null;
+        };
     });
